76e9e0194b3edfaa1a02434fdb4c8ebc557f01f6
GabrielH <>
2024-11-21 16:42:11 +0800

    更新日志配置，添加时间戳和日志级别信息，提升日志可读性

--- push.py
+++ push.py
@@ -3,7 +3,7 @@ import os
 import requests
 import logging
 
-logging.basicConfig(level=logging.INFO)
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 logger = logging.getLogger(__name__)
 
 class PushNotification:
